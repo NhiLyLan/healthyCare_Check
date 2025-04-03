bde61a126ed13ad6b6f3e6c081d12c5518956e82
NhiLy <nhi.ly@katalon.com>
2025-04-03 15:11:39 +0700

    update azure link

---
+++ Test Suites/healthcare-tests - TS_RegressionTest - 01 02 (1).ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>healthcare-tests - TS_RegressionTest - 01 02 (1)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ec1fe3d0-a340-4542-8904-71a53c58731c</testSuiteGuid>
+   <testCaseLink>
+      <guid>12b1ed24-3d2b-4bed-87e1-7c72470cdce7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases Rename 01 02 (1)/TC1_Verify Successful Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bfa3ee72-1061-11f0-9bb5-8341aea44087</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bfa3eea4-1061-11f0-9bb5-8341aea44087</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>955abe25-4029-43e2-b02f-57315c1ade49</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main Test Cases Rename 01 02 (1)/TC3_Visual Testing Example Rename</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
