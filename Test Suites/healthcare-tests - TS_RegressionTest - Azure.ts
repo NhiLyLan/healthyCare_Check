0d74ba76af60c4bb598461e9e06519d177291743
nhi.ly+abc@katalon.com <katalon_g3_dummy_username>
2025-04-04 10:02:07 +0000

    Delete "New Test Case Folder - 1743760785483" test folder

--- Test Suites/healthcare-tests - TS_RegressionTest - Azure.ts
+++ Test Suites/healthcare-tests - TS_RegressionTest - Azure.ts
@@ -11,30 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>ec1fe3d0-a340-4542-8904-71a53c58731c</testSuiteGuid>
-   <testCaseLink>
-      <guid>12b1ed24-3d2b-4bed-87e1-7c72470cdce7</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/New Test Case Folder - 1743760785483/Common Test Cases/Main Test Cases Rename Azure/TC1_Verify Successful Login Azure</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>bfa3ee72-1061-11f0-9bb5-8341aea44087</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>bfa3eea4-1061-11f0-9bb5-8341aea44087</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>955abe25-4029-43e2-b02f-57315c1ade49</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/New Test Case Folder - 1743760785483/Common Test Cases/Main Test Cases Rename Azure/TC3_Visual Testing Example Azure</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
